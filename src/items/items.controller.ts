8f98d6338eac5362c2264cdf4a4c15097e955d61
Ivan <ivan1501@gmail.com>
2019-12-06 16:28:17 +0100

    Changes to auth and users

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,20 @@
-import {Body,Controller,Delete,Get,Param,Post,Put,UseGuards} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Items } from '../items';
 import { Item } from '../item';
 import { AuthGuard } from '@nestjs/passport';
-  
+import { Permissions } from '../permissions.decorator';
+import { PermissionsGuard } from '../permissions.guard';
+
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
@@ -18,20 +29,23 @@ export class ItemsController {
     return this.itemsService.find(id);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
   @Post()
+  @Permissions('create:items')
   create(@Body('item') item: Item) {
     this.itemsService.create(item);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
   @Put()
+  @Permissions('update:items')
   update(@Body('item') item: Item) {
     this.itemsService.update(item);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
   @Delete(':id')
+  @Permissions('delete:items')
   delete(@Param('id') id: number) {
     this.itemsService.delete(id);
   }

99173d16ebedad7185a0b50022fd9a9e9370623f
Ivan <ivan1501@gmail.com>
2019-12-06 14:43:48 +0100

    Added auth and secirity

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,34 +1,38 @@
-import {Body, Controller, Delete, Get, Param, Post,Put} from '@nestjs/common';
-  import { ItemsService } from './items.service';
-  import { Items } from '../items';
-  import { Item } from '../item';
+import {Body,Controller,Delete,Get,Param,Post,Put,UseGuards} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Items } from '../items';
+import { Item } from '../item';
+import { AuthGuard } from '@nestjs/passport';
   
-  @Controller('items')
-  export class ItemsController {
-    constructor(private readonly itemsService: ItemsService) {}
-  
-    @Get()  //Get all items
-    async findAll(): Promise<Items> {
-      return this.itemsService.findAll();
-    }
-  
-    @Get(':id') //Get a single item using an id parameter
-    async find(@Param('id') id: number): Promise<Item> {
-      return this.itemsService.find(id);
-    }
-  
-    @Post() //Create and item
-    create(@Body('item') item: Item) {
-      this.itemsService.create(item);
-    }
-  
-    @Put()  //Update an item
-    update(@Body('item') item: Item) {
-      this.itemsService.update(item);
-    }
-  
-    @Delete(':id')  //Delete an item using id parameter
-    delete(@Param('id') id: number) {
-      this.itemsService.delete(id);
-    }
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  async findAll(): Promise<Items> {
+    return this.itemsService.findAll();
   }
+
+  @Get(':id')
+  async find(@Param('id') id: number): Promise<Item> {
+    return this.itemsService.find(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  create(@Body('item') item: Item) {
+    this.itemsService.create(item);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put()
+  update(@Body('item') item: Item) {
+    this.itemsService.update(item);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    this.itemsService.delete(id);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,14 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: true,
     }),
   );
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
